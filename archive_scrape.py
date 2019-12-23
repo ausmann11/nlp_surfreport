ad71f1525d075651f36d34d7629990b0e790844c
ausmann11 <58743291+ausmann11@users.noreply.github.com>
2019-12-23 11:58:03 -0500

    Update archive_scrape.py

--- archive_scrape.py
+++ archive_scrape.py
@@ -2,15 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
+import time as timez
 
 report_df=pd.DataFrame(columns=['date','part_of_day','time','size','surface','temp','wind','high','low','rise',
                                 'sunset','text'])
 
-for x in range(10):
-    url="http://wblivesurf.com/reports/c-street-"+str(10+x)
+for x in range(10,30):
+    url="http://wblivesurf.com/reports/c-street-{}".format(x)
     r=requests.get(url)
     soup=BeautifulSoup(r.content,'html.parser')
-
+    '''
+    error=soup.find('h2').text
+    skip='Error 404 - Page Not Found'
+    if error == skip:
+        continue
+    '''
     conditions={}
     for strong_tag in soup.find_all('strong'):
         conditions.update({strong_tag.text:strong_tag.next_sibling})
@@ -39,14 +45,18 @@ for x in range(10):
     pattern="g\>.+g\>([a-zA-Z].+)\</"
     text=re.findall(pattern,report)
 
-    report_dict={'date':date,'part_of_day':when[0],'time':time,'size':size,'surface':surface,'temp':temp,'wind':wind,
-             'high':high,'low':low,'rise':rise,'sunset':sunset,'text':text}
+    report_dict={'date':date[0],'part_of_day':when[0],'time':time[0],'size':size,'surface':surface,'temp':temp,
+                'wind':wind,'high':high,'low':low,'rise':rise,'sunset':sunset,'text':text}
 
 
-    temp_df=pd.DataFrame(report_dict)
+    temp_df=pd.DataFrame(report_dict)#,index=[0])
 
-    report_df.append(temp_df)
+    report_df=report_df.append(temp_df)
+
+    timez.sleep(20)
 
 
 
+report_df.reset_index(inplace=True,drop=True)
+len(report_df)
 report_df
